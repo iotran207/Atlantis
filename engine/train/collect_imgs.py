b4560b260d32a83b957537f0a22240adf72143e5
iotran207 <lthlht2007@gmail.com>
2024-05-15 08:50:17 +0700

    update new example models and add vietnamese text

--- engine/train/collect_imgs.py
+++ engine/train/collect_imgs.py
@@ -7,7 +7,7 @@ DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-number_of_classes = 5
+number_of_classes = 1000
 
 dataset_size = 100
 
--- engine/train/inference_classifier.py
+++ engine/train/inference_classifier.py
@@ -5,6 +5,8 @@ import mediapipe as mp
 import numpy as np
 import time
 
+from PIL import Image,ImageDraw,ImageFont
+
 model_dict = pickle.load(open('./model.p', 'rb'))
 model = model_dict['model']
 
@@ -17,7 +19,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 OUTPUT = []
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
-labels_dict = {0:'ok',1:'xin chao',2:'tam biet'}
+labels_dict = {0:'xin chào',1:'giám kháo',2:'tôi là',3:'Bảo Anh',4:'đây là',5:'Dự án mã nguồn mở Atlantis engine'}
 CHECK_FRAME = 0
 predicted_character = ''
 
@@ -49,18 +51,22 @@ while True:
                     mp_drawing_styles.get_default_hand_connections_style())
 
             for hand_landmarks in results.multi_hand_landmarks:
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
+                if hand_landmarks == results.multi_hand_landmarks[0]:
+                    for i in range(len(hand_landmarks.landmark)):
+                        x = hand_landmarks.landmark[i].x
+                        y = hand_landmarks.landmark[i].y
 
-                    x_.append(x)
-                    y_.append(y)
+                        x_.append(x)
+                        y_.append(y)
 
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-                    data_aux.append(x - min(x_))
-                    data_aux.append(y - min(y_))
+                    for i in range(len(hand_landmarks.landmark)):
+                        x = hand_landmarks.landmark[i].x
+                        y = hand_landmarks.landmark[i].y
+
+                        data_aux.append(x - min(x_))
+                        data_aux.append(y - min(y_))
+                else:
+                    pass # đang cập nhật thêm cho nhiều tay
 
             x1 = int(min(x_) * W) - 10
             y1 = int(min(y_) * H) - 10
@@ -70,13 +76,17 @@ while True:
 
             prediction = model.predict([np.asarray(data_aux)])
 
-            print(prediction)
-
             predicted_character = labels_dict[int(prediction[0])]
 
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-            cv2.putText(frame, predicted_character  , (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
-                        cv2.LINE_AA)
+            pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            draw = ImageDraw.Draw(pil_image)
+            font = ImageFont.truetype("Roboto-Regular.ttf", 32) 
+            text_width = font.getlength(predicted_character)
+            text_height = font.getbbox(predicted_character)[3] - font.getbbox(predicted_character)[1]
+            text_x = x1  
+            text_y = y1 - 10 - text_height
+            draw.text((text_x, text_y), predicted_character, font=font, fill=(0, 0, 0))
+            frame = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
 
     if predicted_character != '':
         if len(OUTPUT) == 0 or OUTPUT[-1] != predicted_character:
